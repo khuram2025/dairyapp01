7b0a5e2b721da3250360fa985677eb79759e456f
Khuram Khan <khuram2025@gmail.com>
2021-07-28 14:42:13 +0300

    FireBase Setup Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:diary_app01/screens/main_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -16,7 +20,35 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         primarySwatch: Colors.green,
       ),
-      home: MainPage(),
+      home: GetInfo(),
+    );
+  }
+}
+
+class GetInfo extends StatelessWidget {
+  const GetInfo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('diaries').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text("SomeThing went Wrog");
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+        return new ListView(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+            return new ListTile(
+              title: new Text(document.get('display')),
+              subtitle: new Text(document.get('profession')),
+            );
+          }).toList(),
+        );
+      },),
     );
   }
 }
